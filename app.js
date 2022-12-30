b89baf7123dc55e2bb7d86fabfa27984163a8ac8
nississippi <108444184+nississippi@users.noreply.github.com>
2022-12-30 07:41:15 +0000

    JavaScriptの基本文法

---
+++ app.js
@@ -0,0 +1,50 @@
+// let hello = 'Hello World';
+// alert(hello);
+
+// let str1 = 'Hello';
+// let str2 = 'World!!';
+// alert(str1 + str2);
+
+// let orange = 120;
+// let apple = 100;
+
+// if(orange < apple){
+//   alert('みかんの値段がりんごより安い');
+// } else if(orange == apple){
+//   alert('みかんとりんごが同じ値段');
+// } else{
+//   alert('みかんの値段がりんごより高い');
+// }
+
+// let max = 0;
+// let num = 1;
+// let count = 0;
+
+// while(num < max){
+//   num = num * 2;
+//   count = count + 1;
+// }
+
+// do{
+//   num = num * 2;
+//   count = count + 1;
+// }while(num < max);
+
+// alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+// let i;
+// let num = 0;
+
+// for(i = 1; i < 11; i++){
+//   num = num + i;
+// }
+
+// alert('1から10まで足し算した結果は' + num + 'です');
+
+let int1 = 7
+let int2 = 3
+
+alert(int1 + int2)
+alert(int1 - int2)
+alert(int1 * int2)
+alert(int1 / int2)
