5e5d82bdb0fd659fe64f8daa86a3d7f47c4ede12
nississippi <108444184+nississippi@users.noreply.github.com>
2023-01-03 08:02:56 +0000

    thisとchildren

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
-// $(function() {
-//   $('.box1').mouseover(function () {
-//     $('.box1').css({'background-color': '#0000FF'});
+// $(function(){
+//   $('.bg1').on('click', function(){
+//     $('.bg1').slideUp();
 //   });
-//   $('.box1').mouseout(function () {
-//     $('.box1').css({'background-color': '#FF0000'});
+
+//   $('.bg2').on('click', function(){
+//     $('.bg2').slideUp();
+//   });
+
+//   $('.bg3').on('click', function(){
+//     $('.bg3').slideUp();
+//   });
+
+//   $('.bg4').on('click', function(){
+//     $('.bg4').slideUp();
 //   });
 // });
 
-// $(function () {
-//   $('.box1').mouseover(function (){
-//     $('.box1').addClass('box1-ext');
-//   });
-//   $('.box1').mouseout(function (){
-//     $('.box1').removeClass('box1-ext');
+// $(function(){
+//   $('.box1').on('click', function(){
+//     $(this).slideUp();
 //   });
 // });
 
-$(function () {
-  $('.box1').on('click',function () {
-    $('.box1').addClass('box1-ext');
+$(function(){
+  $('button').on('click', function(){
+    $('ul').children().css('color','red');
   });
-  $('.box1').mouseout(function (){
-    $('.box1').removeClass('box1-ext');
-  });
-})
+});
