af84d8ed749a85870856f8415971ed688aa4819c
nississippi <108444184+nississippi@users.noreply.github.com>
2023-01-03 05:06:00 +0000

    イベントを検出しよう

--- app.js
+++ app.js
@@ -1,45 +1,26 @@
-$(function(){
-  $('.box1').slideDown(function(){
-    $('.box1').css({
-      'background-color': '#0000FF',
-      'width': '200px',
-      'height': '100px'
-    }).slideUp();
+// $(function() {
+//   $('.box1').mouseover(function () {
+//     $('.box1').css({'background-color': '#0000FF'});
+//   });
+//   $('.box1').mouseout(function () {
+//     $('.box1').css({'background-color': '#FF0000'});
+//   });
+// });
+
+// $(function () {
+//   $('.box1').mouseover(function (){
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function (){
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
+
+$(function () {
+  $('.box1').on('click',function () {
+    $('.box1').addClass('box1-ext');
   });
-});
-
-// $(document).ready(function () {
-//   $('body').html('<h1>Hello jQuery!!</h1>');
-// });
-
-// 上記の省略形
-// $(function(){
-//   // jQueryプログラムの内容
-// });
-
-// $(function(){
-//   $('.box1').css({
-//     'background-color': '#0000FF',
-//     'height': '100px'
-//   });
-// });
-
-// $(function(){
-//   $('.box1').slideDown();
-// });
-
-// $(function(){
-//   $('.box1').show();
-//   $('.box1').css({
-//     'background-color': '#0000FF',
-//     'height': '100px'
-//   });
-// });
-
-// $(function(){
-//   $('.box1').slideUp();
-// });
-
-// $(function(){
-//   $('.box1').hide();
-// });
+  $('.box1').mouseout(function (){
+    $('.box1').removeClass('box1-ext');
+  });
+})
