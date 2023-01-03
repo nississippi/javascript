6dc0885a46a5247591668ca7b97344d0b4f293ed
nississippi <108444184+nississippi@users.noreply.github.com>
2023-01-03 04:49:06 +0000

    jQueryでHTMLのCSS要素を操作しよう

--- app.js
+++ app.js
@@ -1,64 +1,45 @@
-// じゃんけんの手を入力してもらうプロンプト欄を生成
-let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
-while ((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)){
-  alert("グー・チョキ・パーのいずれかを入力してください");
-  user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
-}
+$(function(){
+  $('.box1').slideDown(function(){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp();
+  });
+});
 
-// じゃんけんの手をランダムに作成する関数を呼び出す
-let js_hand = getJShand();
+// $(document).ready(function () {
+//   $('body').html('<h1>Hello jQuery!!</h1>');
+// });
 
-// ユーザの手とJavaScriptのじゃんけんの手を比べる関数を呼び出し、結果をjudgeに入れる
-let judge = winLose(user_hand, js_hand);
+// 上記の省略形
+// $(function(){
+//   // jQueryプログラムの内容
+// });
 
-// 結果を表示する
-if (user_hand != null) {
-  alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
-} else {
-  alert("またチャレンジしてね");
-}
+// $(function(){
+//   $('.box1').css({
+//     'background-color': '#0000FF',
+//     'height': '100px'
+//   });
+// });
 
-// ランダムでじゃんけんの手を作成する関数
-function getJShand(){
-  let js_hand_num = Math.floor(Math.random()*3)
-  let hand;
-  if (js_hand_num == 0) {
-    hand="グー";
-  } else if (js_hand_num == 1){
-    hand="チョキ";
-  } else if (js_hand_num == 2){
-    hand="パー";
-  }
-  return hand;
-}
+// $(function(){
+//   $('.box1').slideDown();
+// });
 
-// ユーザの手とJavaScriptのじゃんけんの手を比べる関数
-function winLose(user, js){
-  let winLoseStr;
-  if (user == "グー"){
-    if (js == "グー"){
-      winLoseStr = "あいこ";
-    } else if (js == "チョキ"){
-      winLoseStr = "勝ち";
-    } else if (js == "パー"){
-      winLoseStr = "負け";
-    }
-  } else if (user == "チョキ"){
-    if (js == "グー"){
-      winLoseStr = "負け";
-    } else if (js == "チョキ"){
-      winLoseStr = "あいこ";
-    } else if (js == "パー"){
-      winLoseStr = "勝ち";
-    }
-  } else if (user == "パー"){
-    if (js == "グー"){
-      winLoseStr = "勝ち";
-    } else if (js == "チョキ"){
-      winLoseStr = "負け";
-    } else if (js == "パー"){
-      winLoseStr = "あいこ";
-    }
-  }
-  return winLoseStr;
-}
+// $(function(){
+//   $('.box1').show();
+//   $('.box1').css({
+//     'background-color': '#0000FF',
+//     'height': '100px'
+//   });
+// });
+
+// $(function(){
+//   $('.box1').slideUp();
+// });
+
+// $(function(){
+//   $('.box1').hide();
+// });
