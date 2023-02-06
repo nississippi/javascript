a7a7a4965e77e16b344ae547513c6f5154d72920
nississippi <108444184+nississippi@users.noreply.github.com>
2023-02-06 15:05:13 +0000

    ハイライトつける

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
 $(function() {
-  // ブルー
-  $("#bgBtn1").click(function() {
-    // cssで背景色をブルーに変更
-    $("#bgBox").css("background-color","skyblue");
-  });
-  // ピンク
-  $("#bgBtn2").click(function() {
-    // cssで背景色をピンクに変更
-    $("#bgBox").css("background-color","pink");
-  });
-  // リセット
-  $("#bgBtn3").click(function() {
-    // cssで背景色をデフォルトに変更
-    $("#bgBox").css("background-color","#CCC");
+  $("p").click(function() {
+    // cssでテキスト上に背景色をon/off
+    $(this).toggleClass("hLight");
   });
 });
