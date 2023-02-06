1f485f79a39ebe6eb65a62e395cdc01004f3427d
nississippi <108444184+nississippi@users.noreply.github.com>
2023-02-06 14:58:48 +0000

    文字化け解除

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-function check(){
-  if (mail_form.mail.value == ""){
-      //条件に一致する場合(メールアドレスが空の場合)
-      alert("メールアドレスを入力してください");    //エラーメッセージを出力
-      return false;    //送信ボタン本来の動作をキャンセルします
-  }else{
-      //条件に一致しない場合(メールアドレスが入力されている場合)
-      return true;    //送信ボタン本来の動作を実行します
-  }
-};
+$(function() {
+  // ブルー
+  $("#bgBtn1").click(function() {
+    // cssで背景色をブルーに変更
+    $("#bgBox").css("background-color","skyblue");
+  });
+  // ピンク
+  $("#bgBtn2").click(function() {
+    // cssで背景色をピンクに変更
+    $("#bgBox").css("background-color","pink");
+  });
+  // リセット
+  $("#bgBtn3").click(function() {
+    // cssで背景色をデフォルトに変更
+    $("#bgBox").css("background-color","#CCC");
+  });
+});
