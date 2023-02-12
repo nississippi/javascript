d242bd35d466f348f341b799be127f9568764def
nississippi <108444184+nississippi@users.noreply.github.com>
2023-02-12 11:35:50 +0000

    TODOアプリコメント入力　--date=Feb 11 23:59:59 2023 +0900

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const form = document.getElementById("form");
 const input = document.getElementById("input")
 
 form.addEventListener("submit",function (event){
+  // デフォルトのイベントが発生しなくなる
   event.preventDefault();
   console.log(input.value);
 })
